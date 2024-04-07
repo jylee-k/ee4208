4badc2321fd624cb4a149a90407cd3cab62694de
jlee1255 <jaron8776@gmail.com>
2024-04-07 21:10:00 +0800

    commit

--- assignment_2/canny.py
+++ assignment_2/canny.py
@@ -1,15 +1,22 @@
+import cv2
+import numpy as np
 
-import cv2 as cv
-from matplotlib import pyplot as plt
+image = cv2.imread("data/395.bmp", cv2.IMREAD_GRAYSCALE)
+edges = cv2.Canny(image, 200, 250)
 
+cv2.imshow("Original vs. Canny Edge Detection", np.hstack((image, edges)))
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-img = cv.imread('data/395.bmp', cv.IMREAD_GRAYSCALE)
-assert img is not None, "file could not be read, check with os.path.exists()"
-edges = cv.Canny(img,100,200)
+# import cv2 as cv
+# from matplotlib import pyplot as plt
 
+# img = cv.imread('data/395.bmp', cv.IMREAD_GRAYSCALE)
+# assert img is not None, "file could not be read, check with os.path.exists()"
+# edges = cv.Canny(img,100,200)
 
-plt.subplot(121),plt.imshow(img,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-plt.show()
+# plt.subplot(121),plt.imshow(img,cmap = 'gray')
+# plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+# plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+# plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+# plt.show()
--- assignment_2/sobel.py
+++ assignment_2/sobel.py
@@ -1,22 +1,33 @@
+import cv2
+import numpy as np
 
-import cv2 as cv
+image = cv2.imread("data/395.bmp", cv2.IMREAD_GRAYSCALE)
+sobelx = cv2.Sobel(image, cv2.CV_64F, 1, 0, ksize=5)
+sobely = cv2.Sobel(image, cv2.CV_64F, 0, 1, ksize=5)
+combined_sobel = cv2.addWeighted(sobelx, 0.5, sobely, 0.5, 0)
 
+cv2.imshow("Original", image)
+cv2.imshow("Sobel X", sobelx)
+cv2.imshow("Sobel Y", sobely)
+cv2.imshow("Sobel combined", combined_sobel)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
-img = cv.imread('data/395.bmp', cv.IMREAD_GRAYSCALE)
-assert img is not None, "file could not be read, check with os.path.exists()"
-ksize = 3
-gX = cv.Sobel(img, ddepth=cv.CV_32F, dx=1, dy=0, ksize=ksize)
-gY = cv.Sobel(img, ddepth=cv.CV_32F, dx=0, dy=1, ksize=ksize)
-# the gradient magnitude images are now of the floating point data
-# type, so we need to take care to convert them back a to unsigned
-# 8-bit integer representation so other OpenCV functions can operate
-# on them and visualize them
-gX = cv.convertScaleAbs(gX)
-gY = cv.convertScaleAbs(gY)
-# combine the gradient representations into a single image
-combined = cv.addWeighted(gX, 0.5, gY, 0.5, 0)
-# show our output images
-cv.imshow("Sobel/Scharr X", gX)
-cv.imshow("Sobel/Scharr Y", gY)
-cv.imshow("Sobel/Scharr Combined", combined)
-cv.waitKey(0)
+# img = cv.imread('data/395.bmp', cv.IMREAD_GRAYSCALE)
+# assert img is not None, "file could not be read, check with os.path.exists()"
+# ksize = 3
+# gX = cv.Sobel(img, ddepth=cv.CV_32F, dx=1, dy=0, ksize=ksize)
+# gY = cv.Sobel(img, ddepth=cv.CV_32F, dx=0, dy=1, ksize=ksize)
+# # the gradient magnitude images are now of the floating point data
+# # type, so we need to take care to convert them back a to unsigned
+# # 8-bit integer representation so other OpenCV functions can operate
+# # on them and visualize them
+# gX = cv.convertScaleAbs(gX)
+# gY = cv.convertScaleAbs(gY)
+# # combine the gradient representations into a single image
+# combined = cv.addWeighted(gX, 0.5, gY, 0.5, 0)
+# # show our output images
+# cv.imshow("Sobel/Scharr X", gX)
+# cv.imshow("Sobel/Scharr Y", gY)
+# cv.imshow("Sobel/Scharr Combined", combined)
+# cv.waitKey(0)
