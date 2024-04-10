4e4dc0f473ab489366039b30fa6c9baf7a494be6
LIU-FAYANG <74730293+LIU-FAYANG@users.noreply.github.com>
2024-04-10 19:43:05 +0800

    Add files via upload
    latest version, code cleaning, fixed minor bugs, updated gallery

---
+++ EE4208_Proj1_beta3/FaceRecognition/GUI.py
@@ -0,0 +1,254 @@
+import cv2
+from FaceRecognition.model import FaceID
+import threading, time
+import sys
+import tkinter as tk
+from PIL import Image, ImageTk
+import numpy as np
+
+
+
+class Demonstrator:
+    def __init__(self, gal_dir: str = None, stream_id: int = 0, model_type: str = "mobileNet"):
+        #Model for face recognition 
+        self.FaceID = FaceID(gal_dir=gal_dir, model_type=model_type)
+
+        # Set OpenCV defaults
+        self.color = (0, 0, 255)
+        self.font = cv2.QT_FONT_NORMAL
+
+        #camera_init
+                # camera 
+        self.stream_id = stream_id
+        self.currentFrame = None
+        self.ret = False
+        self.stop = False
+        self.capture = cv2.VideoCapture(stream_id)
+        self.thread = threading.Thread(target=self.update_frame)
+
+        self.flag = False
+        self.stop = False
+
+        self.new_name = 'cat'
+    
+    def update_frame(self):
+        while True:
+            self.ret, self.currentFrame = self.capture.read()
+            while self.currentFrame is None:  # Continually grab frames until we get a good one
+                self.capture.read()
+            if self.stop:
+                break
+
+    # Get current frame
+    def get_frame(self):
+        return self.ret, self.currentFrame
+
+    def screen(self, function):
+        self.thread.start()
+        window_name = "Streaming from {}".format(self.stream_id)
+        cv2.namedWindow(window_name)
+        last = 0
+        while True:
+            ret, frame = self.get_frame()
+            if ret:
+                frame = function(frame)
+                frame = cv2.putText(
+                    frame,
+                    "FPS{:5.1f}".format(1 / (time.time() - last)),
+                    (frame.shape[1] - 80, 30),
+                    cv2.FONT_HERSHEY_PLAIN,
+                    1,
+                    (0, 255, 0),
+                )
+                last = time.time()
+                cv2.imshow(window_name, frame)
+            if cv2.waitKey(1) & 0xFF == ord("q"):
+                break
+        self.stop = True
+        self.thread.join()
+        cv2.destroyWindow(window_name)
+        self.capture.release()
+
+    def annotate(self, img, results):
+        if not results:
+            return img
+        num = 0
+        for result in results:
+            face, detections, ids = result
+            (
+                bbox,
+                points,
+                conf,
+            ) = detections
+            name, gal_face, dist, id_conf = ids
+            
+            # Bbox as int
+            bbox = bbox.astype(int)
+
+            # Point as int
+            points = points.astype(int)
+
+            # Add BoundingBox
+            img = cv2.rectangle(img, tuple(bbox[0]), tuple(bbox[1]), self.color)
+
+            # Add LandmarkPoints
+            for point in points:
+                img = cv2.circle(img, tuple(point), 5, self.color)
+
+            # Add Confidence Value
+            img = cv2.putText(
+                img,
+                "Detect-Conf: {:0.2f}%".format(conf * 100),
+                (int(bbox[0, 0]), int(bbox[0, 1]) - 20),
+                self.font,
+                0.7,
+                (0, 0, 255),
+            )
+
+            img = cv2.putText(
+                img, "{}".format(name), (int(bbox[0, 0]), int(bbox[0, 1]) - 80), self.font, 0.7, (255, 255, 0)
+            )
+            img = cv2.putText(
+                img,
+                "Emb-Dist: {:0.2f}".format(dist),
+                (int(bbox[0, 0]), int(bbox[0, 1] - 60)),
+                self.font,
+                0.7,
+                self.color,
+            )
+            img = cv2.putText(
+                img,
+                "ID-Conf: {:0.2f} %".format(id_conf * 100),
+                (int(bbox[0, 0]), int(bbox[0, 1] - 40)),
+                self.font,
+                0.7,
+                self.color,
+            )
+
+            # Add matched face onto img
+            if name != "Other":
+                img[0 : 0 + gal_face.shape[1], num : num + gal_face.shape[0]] = gal_face
+                # Match
+                img = cv2.putText(img, "GalleryMatch", (num, 112 + 10), self.font, 0.4, (255, 255, 255))
+
+            num += 112
+        return img
+
+    def identification(self, frame):
+        if self.FaceID.recognize_faces(frame):
+            results,  other_ids = self.FaceID.recognize_faces(frame)
+        else:
+            results, other_ids = None, []
+        frame = self.annotate(frame, results)
+
+        return frame, other_ids
+        #return frame
+
+    def run(self):
+        self.screen(self.identification)
+
+    def show(self):
+
+        camIndex = 0
+        self.cap = cv2.VideoCapture(camIndex)
+        success, frame = self.cap.read()
+        if not success:
+            # if camIndex == 0:
+            print("Error, No webcam found!")
+            sys.exit(1)
+
+        mainWindow = tk.Tk()
+        mainWindow.resizable(width=False, height=False)
+        mainWindow.title("Face Detection and Recognition")
+        
+        mainWindow.bind('<Escape>', lambda e: mainWindow.quit())
+        lmain = tk.Label(mainWindow, compound=tk.CENTER, anchor=tk.CENTER, relief=tk.RAISED)
+
+        #define the name entry box and confirm button
+        #used in self.show_entry 
+        self.mainWindow = mainWindow
+        self.lmain = lmain        
+        self.name_entry = tk.Entry(self.mainWindow)
+        self.confirm_button = tk.Button(self.mainWindow, text="Confirm", command=self.add_to_gallery)
+
+        self.button = tk.Button(mainWindow, text="Add person", command=self.show_entry, state=tk.DISABLED)
+        self.button_quit = tk.Button(mainWindow, text="Quit", command=mainWindow.quit)
+
+        lmain.pack()
+        self.button.place(bordermode=tk.INSIDE, relx=0.8, rely=0.1, anchor=tk.CENTER, width=300, height=50)
+        self.button_quit.place(bordermode=tk.INSIDE, relx=0.8, rely=0.8, anchor=tk.CENTER, width=300, height=50)
+        self.button.focus()
+        
+        self.show_frame()
+        mainWindow.mainloop()
+    
+    def show_frame(self):
+    
+        _, frame = self.cap.read()
+        
+        frame, other_ids = self.identification(frame)
+        
+        self.flag = True
+        self.other_ids = other_ids
+
+        
+        
+
+        if self.flag:
+            self.button.configure(state=tk.NORMAL)
+
+        else:
+            self.button.configure(state=tk.DISABLED)
+
+        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+    
+        self.prevImg = Image.fromarray(cv2image)
+        imgtk = ImageTk.PhotoImage(image=self.prevImg)
+        self.lmain.imgtk = imgtk
+        self.lmain.configure(image=imgtk)
+        
+        if not self.stop:
+            self.lmain.after(10, self.show_frame)
+
+
+    def show_entry(self):
+
+        #make 'add person' button disappear on click 
+        self.button.place_forget()
+
+        #define add another person button
+        #it only shows up after click the 'add person' so I just put it here.
+        #on click go back to show_entry
+        self.button2 = tk.Button(self.mainWindow, text="Add another person", command=self.show_entry)
+        self.button2.place(anchor=tk.CENTER, relx=0.8, rely=0.3, width=150, height=50)
+
+        #place name entry box and confirmation button
+        self.name_entry.place(anchor=tk.CENTER, relx=0.8, rely=0.1, width=150, height=50)
+        self.name_entry.focus()
+
+        self.confirm_button.place(anchor=tk.CENTER, relx=0.8, rely=0.2, width=150, height=50)
+            
+
+
+        
+
+    def add_to_gallery(self):
+        # new_name = 'cat'
+        name = self.name_entry.get()
+        print(name)
+        self.new_name = name
+            
+        self.name_entry.place_forget()
+        self.confirm_button.place_forget()
+
+        if self.other_ids:
+            new_emb, new_face = self.other_ids[0]
+
+        #if new_emb:
+            self.FaceID.gal_embs = np.vstack([self.FaceID.gal_embs,new_emb])
+            self.FaceID.gal_names.append(self.new_name)
+            self.FaceID.gal_faces.append(new_face)
+
+            print('Add successful!!!')
+
+        print(len(self.FaceID.gal_embs))
